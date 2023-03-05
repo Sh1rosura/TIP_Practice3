1fec0cd3bcf909f955f6f2f20c272cf67550874b
Sh1rosura <125453763+Sh1rosura@users.noreply.github.com>
2023-03-05 23:24:24 +0300

    Add files via upload

---
+++ Number 2/maincode.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "rational.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int size;
+	cout << "Введите количество дробей -> "; cin >> size;
+	cout << "\n";
+	rational* arr = new rational[size];
+	int a, b;
+
+	cout << "Вводить числа нужно строго через пробел\n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Введите числитель и знаминатель дроби номер " << i + 1 << " : "; cin >> a >> b;
+		(arr[i]).set(a, b);
+	}
+	cout << "\n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << "\nДробь номер " << i + 1 << " : ";
+		(arr[i]).show();
+	}
+	cout << "\n";
+
+	delete[] arr;
+
+	return 0;
+}
